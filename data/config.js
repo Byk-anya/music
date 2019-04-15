c3719b2d28e77e72f24a4f80eb9bb5a170996e3c
Bykova Anna <byk-anya@yandex.ru>
2019-04-15 23:29:26 +0300

    Изменены настройки БД

--- data/config.js
+++ data/config.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 // Set database connection credentials
 const config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'api',
+    host: 'ec2-54-228-252-67.eu-west-1.compute.amazonaws.com',
+    user: 'ntpyhvvoiapxew',
+    password: '8295ccc429a9e0a866ae2abc0b3c25dba047cf8cdcfc4aeda8ee1e69a728b451',
+    database: 'd3ga8vvfeak3hh',
 };
 
 
